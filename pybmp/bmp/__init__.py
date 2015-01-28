d8ccba9b0d5eac286df3ecd6b8bb41676e43aada
Paul Hobson <pmhobson@gmail.com>
2015-01-28 13:45:46 -0800

    ENH: added parameterBoxplots and bmpStatSummart to import

--- pybmp/bmp/__init__.py
+++ pybmp/bmp/__init__.py
@@ -4,7 +4,8 @@ from .summary import (
     DatasetSummary,
     getSummaryData,
     setMPLStyle,
-    parameterBoxplots
+    parameterBoxplots,
+    bmpStatSummary
 )
 
 from ..testing import NoseWrapper
