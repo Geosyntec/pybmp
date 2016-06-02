6f73cbd719456771da1f55fd7f5c843614504cda
Paul Hobson <pmhobson@gmail.com>
2016-06-02 10:10:58 -0700

    remove notebook styling function

--- pybmpdb/__init__.py
+++ pybmpdb/__init__.py
@@ -7,11 +7,3 @@ from .summary import (
     parameterBoxplots,
     bmpStatSummary
 )
-
-
-def style_notebook(filepath=None):
-    from IPython.core.display import HTML
-    if filepath is None:
-        filepath = "../styles/ipynb.css"
-    styles = open(filepath, "r").read()
-    return HTML(styles)
