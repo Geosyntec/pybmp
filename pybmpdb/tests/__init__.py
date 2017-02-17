dfe9b39dacca30cfc69f7f179d9a1525014f95b2
Paul Hobson <pmhobson@gmail.com>
2017-02-17 15:56:50 -0800

    make pytest optional

--- pybmpdb/tests/__init__.py
+++ pybmpdb/tests/__init__.py
@@ -1,10 +1,14 @@
 from pkg_resources import resource_filename
 
-import pytest
-
 import pybmpdb
 
+
 def test(*args):
+    try:
+        import pytest
+    except ImportError:
+        raise ImportError("pytest required run tests")
+
     options = [resource_filename('pybmpdb', 'tests')]
     options.extend(list(args))
     return pytest.main(options)
